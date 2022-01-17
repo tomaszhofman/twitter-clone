39a49df9c52a9b29895408124a0ca9a834bca479
Tomasz Hofman <tomasz.k.hofman@gmail.com>
2022-01-17 13:52:42 +0100

    Add profile menu

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { SidebarLink } from '@/components/Sidebar/SidebarLink';
 import { NAVIGATION_LINKS } from '@/components/Sidebar/navMenu';
 import { useRouter } from 'next/router';
+import { DotsCircleHorizontalIcon } from '@heroicons/react/outline';
 
 function Sidebar() {
   const router = useRouter();
@@ -33,6 +34,19 @@ function Sidebar() {
       <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
         Tweet
       </button>
+
+      <div className=" text-white flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5 w-56">
+        <img
+          src="https://pbs.twimg.com/profile_images/1467773673058750468/X-7z8YWX_x96.png"
+          alt=""
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">Tomasz</h4>
+          <p className="text-gray-300">@tomasttttttz</p>
+        </div>
+        <DotsCircleHorizontalIcon className="hidden h-5 xl:inline ml-10" />
+      </div>
     </div>
   );
 }
