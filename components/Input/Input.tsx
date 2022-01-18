111251c707faade5cff3e68f04cd1fd12f22d8d3
Tomasz Hofman <tomasz.k.hofman@gmail.com>
2022-01-18 17:58:36 +0100

    Add emoji picker

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -2,9 +2,13 @@ import React, { ChangeEvent, useRef, useState } from 'react';
 import Image from 'next/image';
 import { Icon } from '@/components/Icon';
 import { IconTypesEnum } from '../../types/iconsTypes';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 function Input() {
   const [inputValue, setInputValue] = useState('');
+  const [selectImage] = useState('');
+  const [showEmoji, setShowEmoji] = useState(false);
   const filePickerRef = useRef(null);
 
   const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -12,6 +16,17 @@ function Input() {
   };
 
   const addImageToPostHandler = () => {};
+
+  const sendPostHandler = () => {};
+
+  const addEmojiHandler = (e: { unified: string }) => {
+    let sym = e.unified.split('-');
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push('0x' + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInputValue(inputValue + emoji);
+  };
+
   return (
     <div className="w-full border-b border-[#2F3336] p-3 flex space-x-3 overflow-y-scroll items-center">
       <div>
@@ -46,6 +61,7 @@ function Input() {
                 type="file"
                 className="hidden"
                 onChange={addImageToPostHandler}
+                value={selectImage}
                 ref={filePickerRef}
               />
             </div>
@@ -56,9 +72,34 @@ function Input() {
               <Icon name={IconTypesEnum.Survey} className="h-22 " />
             </div>
             <div className="icon">
-              <Icon name={IconTypesEnum.Emoji} className="h-22 " />
+              <Icon
+                onClick={() => setShowEmoji(!showEmoji)}
+                name={IconTypesEnum.Emoji}
+                className="h-22 "
+              />
             </div>
+
+            {showEmoji && (
+              <Picker
+                onSelect={addEmojiHandler}
+                style={{
+                  position: 'absolute',
+                  marginTop: '465px',
+                  marginLeft: -40,
+                  maxWidth: '320px',
+                  borderRadius: '20px',
+                }}
+                theme="dark"
+              />
+            )}
           </div>
+          <button
+            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+            disabled={!inputValue.trim() && !selectImage}
+            onClick={sendPostHandler}
+          >
+            tweet
+          </button>
         </div>
       </div>
     </div>
